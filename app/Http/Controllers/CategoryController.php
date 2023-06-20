97a4069f3d51f93718b836c363e6c29e408115f1
reiji <ivan2005verem@gmail.com>
2023-06-20 20:15:49 +0300

    d

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,49 +6,156 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     tags={"Category"},
+     *     path="/api/category",
+     *     @OA\Response(response="200", description="List Categories.")
+     * )
+     */
     public function index() {
         $list = Category::all();
         return response()->json($list, 200,
             ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
     }
-    public function store(Request $request)
-    {
-        // Отримати дані з запиту і зберегти нову категорію
-        $category = new Category;
-        $category->name = $request->input('name');
-        // Збереження інших полів категорії
-        $category->save();
-
-        return response()->json(['message' => 'Категорія успішно створена']);
+    /**
+     * @OA\Post(
+     *     tags={"Category"},
+     *     path="/api/category",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 required={"name","image","description"},
+     *                 @OA\Property(
+     *                     property="image",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="description",
+     *                     type="string"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Add Category.")
+     * )
+     */
+    public function store(Request $request) {
+        $input = $request->all();
+        $message = array(
+            'name.required'=>"Вкажіть назву категорії",
+            'image.required'=>"Вкажіть фото категорії",
+            'description.required'=>"Вкажіть опис категорії",
+        );
+        $validator = Validator::make($input,[
+            'name'=>'required',
+            'image'=>'required',
+            'description'=>'required'
+        ], $message);
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 400,
+                ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+        }
+        $category = Category::create($input);
+        return response()->json($category, 200,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
     }
 
-    public function update(Request $request, $id)
-    {
-        // Знайти категорію за ідентифікатором
-        $category = Category::find($id);
-        if (!$category) {
-            return response()->json(['message' => 'Категорія не знайдена'], 404);
+    /**
+     * @OA\Post(
+     *     tags={"Category"},
+     *     path="/api/category/edit/{id}",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Ідентифікатор категорії",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="number",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 required={"name"},
+     *                 @OA\Property(
+     *                     property="image",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="description",
+     *                     type="string"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Add Category.")
+     * )
+     */
+    public function update($id, Request $request) {
+        $category = Category::findOrFail($id);
+        $input = $request->all();
+        $message = array(
+            'name.required'=>"Вкажіть назву категорії",
+            'image.required'=>"Вкажіть фото категорії",
+            'description.required'=>"Вкажіть опис категорії",
+        );
+        $validator = Validator::make($input,[
+            'name'=>'required',
+            'image'=>'required',
+            'description'=>'required'
+        ], $message);
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 400,
+                ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
         }
-
-        // Оновити поля категорії
-        $category->name = $request->input('name');
-        // Оновлення інших полів категорії
-        $category->save();
-
-        return response()->json(['message' => 'Категорія успішно оновлена']);
+        $category->update($input);
+        return response()->json($category, 200,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
     }
 
-    public function destroy($id)
-    {
-        // Знайти категорію за ідентифікатором
-        $category = Category::find($id);
-        if (!$category) {
-            return response()->json(['message' => 'Категорія не знайдена'], 404);
-        }
-
-        // Видалити категорію
+    /**
+     * @OA\Delete(
+     *     path="/api/category/{id}",
+     *     tags={"Category"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Ідентифікатор категорії",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="number",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Успішне видалення категорії"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Категорії не знайдено"
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Не авторизований"
+     *     )
+     * )
+     */
+    public function delete(Request $request, $id) {
+        $category = Category::findOrFail($id);
         $category->delete();
-
-        return response()->json(['message' => 'Категорія успішно видалена']);
+        return 204;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::get("/category", [CategoryController::class, "index"]);
 //http://laravel.pv125.com/api/category
-Route::post('/categories', 'CategoryController@store');
-Route::put('/categories/{id}', 'CategoryController@update');
-Route::delete('/categories/{id}', 'CategoryController@destroy');
+Route::post("/category", [CategoryController::class, "store"]);
+Route::post("/category/edit/{id}", [CategoryController::class, "update"]);
+Route::delete("/category/{id}", [CategoryController::class, "delete"]);
